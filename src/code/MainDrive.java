7ca7750112a0c554e58411aabcd6076f05f82fed
seakjh <skykjh0513@gmail.com>
2020-03-06 10:34:43 +0900

    연도 입력기능
    스캐너로 정수값의 년도를 입력 받았다.

---
+++ src/code/MainDrive.java
@@ -0,0 +1,19 @@
+package code;
+
+import java.util.Scanner;
+
+public class MainDrive {
+	
+	public static void main(String[] args) {
+		
+//		인코딩 : UTF-8
+		
+		Scanner scan = new Scanner(System.in);
+		System.out.println("궁금한 년도 입력 : ");
+		int year = scan.nextInt();
+		
+		System.out.println(year);
+		
+	}
+
+}
