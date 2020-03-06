00bd94be88be9a768ecaa2bfcae68a25c2c9fe5e
seakjh <skykjh0513@gmail.com>
2020-03-06 10:37:34 +0900

    처음부터 4로 안나눠지면 윤년이아님 확정

--- src/code/MainDrive.java
+++ src/code/MainDrive.java
@@ -12,7 +12,14 @@ public class MainDrive {
 		System.out.println("궁금한 년도 입력 : ");
 		int year = scan.nextInt();
 		
-		System.out.println(year);
+//		4로 안나눠지면 윤년이 아니라고 확정
+		if (year % 4 == 0) {
+			System.out.println("윤년이 아님");
+			System.out.println("4로 안나눠 떨어짐.");
+		}
+		else {
+//			100으로 나누고
+		}
 		
 	}
 
